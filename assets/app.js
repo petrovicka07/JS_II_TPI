cf402faf0a429aa8733b96ddbf986aabc887bc1a
Patricia <Patricia@test.com>
2023-11-08 13:13:08 +0100

    edit

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+
+
+
+let mojvek= 16;
+let mojemeno= "Patricia";
+
+let mojaadres= "mokros";
+let adresaskoly= "holic";
+
+let meno="patricia";
+let priezviso= "petrovicova"; 
+let mojemeno= "meno+prizvisko";
+
+let ulica="mokry haj";
+let psc="90865";
+let mesto="mokry haj";
+
